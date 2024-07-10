2eb789168abfb0175f2bf3db4b18ff1bad5ae9ef
codeprogl <haarkerishi@gmail.com>
2024-07-10 10:53:43 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -2,16 +2,32 @@ import javax.swing.*;
 
 
 
-class demo extends JFrame{
+class demo{
 
+JFrame f;
+JInternalFrame inf;
+   
+demo(){
+
+   f = new JFrame("Frame");
+   f.setVisible(true);
+   
+   f.setSize(400,400);
+   f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+   inf = new JInternalFrame("");
+   inf.setVisible(true);
+   inf.setLayout(null);
+   f.add(inf);
+
+   
+}
+
+   
 
 public static void main(String[] args){
 
-   setTitle("Demo frame");
-  setVisible(true);
-  setLayout(null);
-
-  
+  new demo();
 
 }
 }
